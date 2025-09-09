faee8b9d2946354f50516692ecd119b522369dfe
ermakgithub <152062275+ermakgithub@users.noreply.github.com>
2025-09-10 00:49:08 +0300

    Vite+Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-black text-white text-4xl font-bold">
+            Tailwind 4 работает 🚀
+        </div>
+    )
+}
+
+export default App
+
+
+
